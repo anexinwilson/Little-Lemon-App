3c3bad641647decd33c3bd0dcf8a388143905eda
anexinwilson <anexin777@gmail.com>
2025-03-30 23:37:37 -0700

    Added tailwind css

---
+++ little-lemon/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 class="text-red-600">Testing</h1>
+    </>
+  )
+}
+
+export default App
